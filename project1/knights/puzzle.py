31bce22bae7ddf69dea5b1a54603fd15580d4020
patmull <patmull@seznam.cz>
2023-08-31 18:17:11 +0200

    Added general knowledge

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -9,46 +9,77 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
-ASaid = Symbol("A said")
-BSaid = Symbol("B Said")
-CSaid = Symbol("C Said")
+general_knowledge_1 = Or(AKnight, AKnave)
+general_knowledge_2 = Or(BKnight, BKnave)
+general_knowledge_3 = Not(And(AKnight, AKnave))
+general_knowledge_4 = Not(And(BKnight, BKnave))
 
 # ASaid = Or(AKnight, AKnave)
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
+ASaid = And(AKnight, AKnave)
+
+general_knowledge_with_sentece_1 = Implication(AKnight, ASaid)
+general_knowledge_with_sentece_2 = Implication(AKnave, Not(ASaid))
+
+general_knowledge = And(
+    general_knowledge_1, general_knowledge_2, general_knowledge_3, general_knowledge_4)
+
 knowledge0 = And(
     # TODO
-    Implication(ASaid, And(AKnight, AKnave))
+    general_knowledge,
+    general_knowledge_with_sentece_1,
+    general_knowledge_with_sentece_2
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+ASaid = And(AKnave, BKnave)
+BSaid = And(Not(BKnave), BKnave)
+
 knowledge1 = And(
+    general_knowledge_1,
+    general_knowledge_2,
+    general_knowledge_3,
+    general_knowledge_4,
     # TODO
-    Implication(ASaid, And(AKnave, BKnave)),
+    ASaid,
     Not(BSaid)
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+ASaid = Biconditional(AKnight, BKnight)
+BSaid = Or((Not(Biconditional(AKnight, BKnight))), And(BSaid, Not(Biconditional(AKnave, BKnave))))
+
 knowledge2 = And(
     # TODO
-    Implication(ASaid, Biconditional(AKnight, BKnight)),
-    )
+    ASaid,
+    BSaid
+)
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+ASaid1 = AKnight  # "I am a knight."
+ASaid2 = AKnave  # "I am a knave."
+ASaid = Or(ASaid1, ASaid2)
+BSaid1 = And(ASaid, AKnave)
+BSaid2 = CKnight
+CSaid = AKnight
+
 knowledge3 = And(
-    And(ASaid, Or(AKnight, AKnave)),
-    And(BSaid, And(ASaid, AKnave)),
-    And(BSaid, Not(CKnave)),
-    And(CSaid, AKnight)
+    ASaid1,
+    ASaid2,
+    ASaid,
+    BSaid1,
+    BSaid2,
+    CSaid
 )
 
 """
@@ -103,7 +134,7 @@ def main():
     for puzzle, knowledge in puzzles:
         print(puzzle)
         if len(knowledge.conjuncts) == 0:
-            print("    Not yet implemented.")
+            print("Not yet implemented.")
         else:
             for symbol in symbols:
                 if model_check(knowledge, symbol):
